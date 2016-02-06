41e1ba40ede07117fd29fed58d4e833c20bc8dcf
latte <latte.h-nd@outlook.com>
2016-02-06 13:32:36 +0700

    Deleted

--- app/src/main/java/latte/nadia/nadiares/MainActivity.java
+++ app/src/main/java/latte/nadia/nadiares/MainActivity.java
@@ -1,5 +1,6 @@
 package latte.nadia.nadiares;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,7 +18,32 @@ public class MainActivity extends AppCompatActivity {
 
         //Request Database
         objMyManage = new MyManage(this);
+
+        //Test add Value
+        //testAddValue();
+
+        //CleanData
+        cleanData();//ตัวแปร
+
     }//Main Method
+
+    private void cleanData() {
+
+        SQLiteDatabase objSQLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        objSQLiteDatabase.delete(MyManage.food_TABLE, null, null);
+        objSQLiteDatabase.delete(MyManage.user_TABLE, null, null);
+
+    }// CleanData
+
+    private void testAddValue() {
+
+        for ( int i=0;i<=1;i++){
+            objMyManage.addNewValue(i,"test1","test2","test3");
+        }// for
+
+    }// testaddvalue
+
 
 }// Main Class
 
--- app/src/main/java/latte/nadia/nadiares/MyManage.java
+++ app/src/main/java/latte/nadia/nadiares/MyManage.java
@@ -1,5 +1,6 @@
 package latte.nadia.nadiares;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -13,6 +14,18 @@ public class MyManage {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String user_TABLE = "userTABLE";
+    public static final String food_TABLE = "foodTABLE";
+    public static final String column_id = "_id";
+    public static final String column_user = "User";
+    public static final String column_pass = "Password";
+    public static final String column_name = "Name";
+    public static final String column_food = "Food";
+    public static final String column_price = "Price";
+    public static final String column_source = "Source";
+
+
+
 
     public MyManage(Context context){
 
@@ -22,5 +35,38 @@ public class MyManage {
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     }//Constructor
+
+
+    public long addNewValue (int intTable,
+                             String strColumn2,
+                             String strColumn3,
+                             String strColumn4){
+
+        ContentValues objContentValues = new ContentValues();
+        long longreturn = 0 ;
+        switch (intTable) {
+            case 0:
+
+                objContentValues.put(column_user,strColumn2);
+                objContentValues.put(column_pass,strColumn3);
+                objContentValues.put(column_name,strColumn4);
+                writeSqLiteDatabase.insert(user_TABLE,null,objContentValues);
+
+
+                break;
+            case 1:
+
+                objContentValues.put(column_food,strColumn2);
+                objContentValues.put(column_price,strColumn3);
+                objContentValues.put(column_source,strColumn4);
+                writeSqLiteDatabase.insert(food_TABLE,null,objContentValues);
+
+                break;
+        }// swich
+
+
+
+        return longreturn;
+    }
 
 }// Main Class
