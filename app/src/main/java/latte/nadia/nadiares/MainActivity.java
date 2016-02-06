43d992cc8e750dfa656e784cae8e59ec5640ae44
latte <latte.h-nd@outlook.com>
2016-02-06 15:05:17 +0700

    Finished 060216

--- app/src/main/java/latte/nadia/nadiares/MainActivity.java
+++ app/src/main/java/latte/nadia/nadiares/MainActivity.java
@@ -1,8 +1,12 @@
 package latte.nadia.nadiares;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +29,46 @@ public class MainActivity extends AppCompatActivity {
         //CleanData
         cleanData();//ตัวแปร
 
+        //Sync JSON to SQLite
+        synJSONtoSQLite();
+
     }//Main Method
+
+    private void synJSONtoSQLite() {
+
+
+        //Change Policy
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myPolicy);
+
+        int intTable = 1;
+        String tag = "Restuarant";
+
+        while (intTable <= 2){
+
+            //1. Create InputStream คือ ไหมพรหมมีแค่ 1 เส้น โหลดไปประมวลผลไป
+            InputStream objInputStream = null;
+            String strURL_user = "http://swiftcodingthai.com/6feb/php_get_nadia.php";
+            String strURL_food = "http://swiftcodingthai.com/6feb/php_get_data_food.php";
+
+            try {
+
+            }catch (Exception e){
+                Log.d(tag,"InputStream ==>"+e.toString());
+            }
+
+
+            //2. Create JSON String สิ่งที่โหลดมา เปลี่ยนเป็น String
+            //3. Update SQLite ขึ้น Lite
+
+            intTable +=1;
+
+
+        }// while
+
+
+    }// synJSONtoSQLite
 
     private void cleanData() {
 
