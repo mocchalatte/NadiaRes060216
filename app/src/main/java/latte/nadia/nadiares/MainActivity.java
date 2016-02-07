6a4e98fbfac20f7809cdf5a6a6503d6a2ce5e5af
latte <latte.h-nd@outlook.com>
2016-02-07 10:36:32 +0700

    Sync JSON to SQLite  แล้ว

--- app/src/main/java/latte/nadia/nadiares/MainActivity.java
+++ app/src/main/java/latte/nadia/nadiares/MainActivity.java
@@ -1,12 +1,22 @@
 package latte.nadia.nadiares;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,10 +59,25 @@ public class MainActivity extends AppCompatActivity {
 
             //1. Create InputStream คือ ไหมพรหมมีแค่ 1 เส้น โหลดไปประมวลผลไป
             InputStream objInputStream = null;
-            String strURL_user = "http://swiftcodingthai.com/6feb/php_get_nadia.php";
-            String strURL_food = "http://swiftcodingthai.com/6feb/php_get_data_food.php";
+            String strURLuser = "http://swiftcodingthai.com/6feb/php_get_nadia.php";
+            String strURLfood = "http://swiftcodingthai.com/6feb/php_get_data_food.php";
 
-            try {
+                HttpPost objHttpPost = null;
+                try {
+                    HttpClient objHttpClient = new DefaultHttpClient();//ขีดกลาง คือ มองว่าไม่ปลอดภัย
+                    switch (intTable) {
+                        case 1:
+                            objHttpPost = new HttpPost(strURLuser);
+                            break;
+                        case 2:
+                            objHttpPost = new HttpPost(strURLfood);
+                            break;
+                        default:
+                            break;
+                    } // switch
+                    HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+                    HttpEntity objHttpEntity = objHttpResponse.getEntity();
+                    objInputStream = objHttpEntity.getContent();
 
             }catch (Exception e){
                 Log.d(tag,"InputStream ==>"+e.toString());
@@ -60,7 +85,68 @@ public class MainActivity extends AppCompatActivity {
 
 
             //2. Create JSON String สิ่งที่โหลดมา เปลี่ยนเป็น String
+            String strJSON = null;
+            try{
+
+
+                BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream,"UTF-8"));//แปลงให้เป็นภาษาไทย
+                StringBuilder objStringBuilder = new StringBuilder();
+                String strLine = null;
+
+                while ((strLine = objBufferedReader.readLine()) != null){
+                    objStringBuilder.append(strLine);
+
+                }//while //StringBuilder เป็นตัวประกอบ String ที่มาสเตอร์ตัดเป็นท่อนๆ
+
+                objInputStream.close();
+                strJSON = objStringBuilder.toString();//strJSON ยาวมาก แค่บรรทัดเดียว
+
+
+
+            }catch (Exception e){
+                Log.d(tag,"strJSON ==>" + e.toString());
+            }
+
             //3. Update SQLite ขึ้น Lite
+            try{
+
+                JSONArray objJsonArray = new JSONArray(strJSON);
+                for(int i=0;i<objJsonArray.length();i++){
+
+                    JSONObject jsonObject = objJsonArray.getJSONObject(i);
+                    switch (intTable){
+                        case 1:
+
+                            //GetValue From userTABLE
+                            String strUser = jsonObject.getString(MyManage.column_user);//,มันวิ่งไป user
+                            String strPassward = jsonObject.getString(MyManage.column_pass);
+                            String strName = jsonObject.getString(MyManage.column_name);
+
+                            objMyManage.addNewValue(0,strUser,strPassward,strPassward);
+
+
+                            break;
+                        case 2:
+
+                            //GetValue From foodTABLE
+                            String strFood = jsonObject.getString(MyManage.column_food);
+                            String strPrice = jsonObject.getString(MyManage.column_price);
+                            String strSource = jsonObject.getString(MyManage.column_source);
+
+                            objMyManage.addNewValue(1,strFood,strPrice,strSource);
+
+
+
+                            break;
+                    }
+
+                }// for
+
+
+
+            }catch (Exception e){
+                Log.d(tag,"Update ==>"+ e.toString());
+            }
 
             intTable +=1;
 
